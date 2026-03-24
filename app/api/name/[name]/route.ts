00234ba58a8ee20b492e143207ebabed8a19ddd5
yash tanwar <yashtanwar810@gmail.com>
2026-03-24 12:38:26 +0530

    fixed build issue

--- app/api/name/[name]/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export const GET = (req: NextApiRequest, res: NextApiResponse, ctx: {params: {name: string}}) => {
-        const {name}  = ctx.params
-
-        return res.send(name)
-}
